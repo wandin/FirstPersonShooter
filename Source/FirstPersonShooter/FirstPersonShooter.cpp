890ae63ad2c6e242de32956473e6204b1809213e
wandin <wanderson.lopes@outlook.com>
2024-06-12 19:29:47 -0300

    Initial Commit FPS working on blueprints first.

---
+++ Source/FirstPersonShooter/FirstPersonShooter.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "FirstPersonShooter.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, FirstPersonShooter, "FirstPersonShooter" );
